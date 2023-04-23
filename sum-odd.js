f72264de493f9f00f19102c7577acddb7fd4d64b
Sahil R <sahilrw000@gmail.com>
2023-04-23 10:08:53 +0530

    sum of odd numbers

---
+++ sum-odd.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Given the triangle of consecutive odd numbers:
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1)
+
+// my solution
+function rowSumOddNumbers(n) {
+	return n * n * n
+}
