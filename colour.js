bc03fb178d6a7b7a451c9fd3307774867b0a881b
Sahil R <sahilrw000@gmail.com>
2023-04-25 19:58:41 +0530

    colour association

---
+++ colour.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// Colour plays an important role in our lifes. Most of us like this colour better then another. User experience specialists believe that certain colours have certain psychological meanings for us.
+// You are given a 2D array, composed of a colour and its 'common' association in each array element. The function you will write needs to return the colour as 'key' and association as its 'value'.
+
+// my solution
+function colourAssociation(array){
+  return array.map(([e, i]) => ({[e]: i}))
+}
+
+// function colourAssociation(array){
+//   let result = []
+//   for(let i = 0; i < array.length; i++){
+//     result[i] = {[array[i][0]]: array[i][1]}
+//   } return result
+// }
