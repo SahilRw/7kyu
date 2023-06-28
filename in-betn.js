9bdb703c6b439c79417282fe613f098ee99f5b56
Sahil R <sahilrw000@gmail.com>
2023-06-28 14:02:13 +0530

    what is in between

---
+++ in-betn.js
@@ -0,0 +1,11 @@
+// Description:
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// my solution:
+function between(a, b) {
+  const result = [];
+  for (let i = a; i <= b; i++) {
+    result.push(i);
+  }
+  return result;
+}
