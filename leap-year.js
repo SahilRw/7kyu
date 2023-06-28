25a7ac411c46131291a653451b4dc4aee10620b0
Sahil R <sahilrw000@gmail.com>
2023-06-28 20:32:52 +0530

    stop spinning my words

--- leap-year.js
+++ leap-year.js
@@ -21,3 +21,12 @@ function isLeapYear(year) {
     return false;
   }
 }
+
+const isLeap = (year) =>
+  year % 400 === 0
+    ? true
+    : year % 100 === 0
+    ? false
+    : year % 4 === 0
+    ? true
+    : false;
