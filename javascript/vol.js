47a62d9eb43eb2f44386cac51cd6344a410d8d11
Sahil R <sahilrw000@gmail.com>
2023-10-18 10:35:12 +0530

    difference of volumes of cuboids(one line solution)

--- javascript/vol.js
+++ javascript/vol.js
@@ -11,3 +11,8 @@ function findDifference(a, b) {
   
   return Math.abs(volA - volB)
 }
+
+// one line solution
+function findDiff(a, b){
+      return Math.abs((a[0] * a[1] * a[2]) - (b[0] * b[1] * b[2]))
+}
