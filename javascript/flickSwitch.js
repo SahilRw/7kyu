9ca0a07f25d8aa87fb3645903dbd9e25c00243b0
Sahil R <sahilrw000@gmail.com>
2024-02-09 23:32:23 +0530

    flick switch

---
+++ javascript/flickSwitch.js
@@ -0,0 +1,27 @@
+// Create a function that always returns True/true for every item in a given list.
+// However, if an element is the word 'flick', switch to always returning the opposite boolean value.
+
+// Examples
+// ['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+
+// ['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+
+// ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+// Notes
+// "flick" will always be given in lowercase.
+// A list may contain multiple flicks.
+// Switch the boolean value on the same element as the flick itself.
+
+// my solution
+function flickSwitch(arr) {
+  let result = [];
+  let value = true;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "flick") {
+      value = !value;
+    }
+    result.push(value);
+  }
+  return result;
+}
